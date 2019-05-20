63c56d6057b3f879f03492322af6df4227d2f41c
ahmedzaidi786 <ahmedzaidi786@users.noreply.github.com>
2019-05-20 14:36:36 -0400

    test on other computer

--- src/main/java/com/marzia/webserver/WebserverApplication.java
+++ src/main/java/com/marzia/webserver/WebserverApplication.java
@@ -3,7 +3,7 @@ package com.marzia.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 
 @SpringBootApplication
--- src/main/java/com/marzia/webserver/controller/HomeController.java
+++ src/main/java/com/marzia/webserver/controller/HomeController.java
@@ -5,15 +5,29 @@ import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.marzia.webserver.dao.repository.PatientVisitMapper;
+import com.marzia.webserver.dao.repository.PatientsMapper;
+import com.marzia.webserver.dao.repository.RateBookMapper;
 
 
 
 @Controller
 public class HomeController {
 	private static Logger log = Logger.getLogger(HomeController.class.getName());
+	
+	@Autowired
+	private PatientsMapper pm;
+	
+	@Autowired
+	private PatientVisitMapper pvm;
+	
+	@Autowired
+	private RateBookMapper rbm;
 	
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home(HttpServletRequest req) {
