7a4dfd3197a3be1829e19ad2250a1cc40ae78257
ahmedzaidi786 <ahmedzaidi786@users.noreply.github.com>
2019-08-13 11:30:42 -0400

    progress 1

--- src/main/java/com/marzia/webserver/controller/HomeController.java
+++ src/main/java/com/marzia/webserver/controller/HomeController.java
@@ -3,11 +3,14 @@ package com.marzia.webserver.controller;
 
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +22,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.marzia.webserver.dao.model.Patients;
+import com.marzia.webserver.dao.model.PatientsExample;
+import com.marzia.webserver.dao.repository.PatientsMapper;
+
+import sun.util.logging.resources.logging;
 
 
 
@@ -26,6 +33,8 @@ import com.marzia.webserver.dao.model.Patients;
 public class HomeController {
 	private static Logger log = Logger.getLogger(HomeController.class.getName());
 
+	@Autowired
+	 private PatientsMapper pm;
 	
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home(HttpServletRequest req) {
@@ -36,31 +45,71 @@ public class HomeController {
 	@RequestMapping(value = "/newPatient", method = RequestMethod.GET)
 	public String newPatient( Model model) {
 		model.addAttribute("newPatient", new Patients());
-		log.info("Firstname =" +model);
 		return "newPatient";
 	}
 	
 	@RequestMapping(value = "/newPatient", method = RequestMethod.POST)
-	public String newPatientSubmit( @Valid Patients patients, BindingResult bindingResult,Model model,@RequestParam("mobile") String mobile,
-			@RequestParam(value="since") @DateTimeFormat(pattern="MMddyyyy") String date ) {
-		log.info("Mobile =" + mobile);
-		log.info("Date =" + date);
-		log.info("Mobile object =" + patients.getMobile());
+	public String newPatientSubmit( @Valid Patients patients, BindingResult bindingResult,Model model,@RequestParam("cell") String cell,
+			@RequestParam(value="since") @DateTimeFormat(pattern="MMddyyyy") String date) {
 		return "result";
 	}
 	
 	@ResponseBody
 	@RequestMapping(value = "/newPatientSubmit", method = RequestMethod.POST)
-	public Patients newPatientSubmitButton( @Valid Patients patients, BindingResult bindingResult,Model model,@RequestParam("mobile") String mobile,
+	public List<Patients> newPatientSubmitButton( @Valid Patients patients, BindingResult bindingResult,Model model,@RequestParam("cell") String cell,
 			@RequestParam(value="since") @DateTimeFormat(pattern="MMddyyyy") String date ) {
-		log.info("Mobile =" + mobile);
-		log.info("Date =" + date);
-		log.info("object =" + patients.toString());
-		return patients;
+		PatientsExample pe = new PatientsExample();
+		pe.createCriteria().andFirstnameEqualTo(patients.getFirstname())
+		.andLastnameEqualTo(patients.getLastname());
+		List<Patients> patient = pm.selectByExample(pe); 
+		if(patient.isEmpty()) {
+			pm.insert(patients);	
+		}
+		
+		return patient;
 	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/patientList", method = RequestMethod.GET)
+	public List<Patients> patientList(
+			) {		
+		PatientsExample pe = new PatientsExample();
+		pe.createCriteria().getAllCriteria();
+		List<Patients> patient = pm.selectByExample(pe); 
+		return patient;
+	
+	}
+	
 	@RequestMapping(value = "/logOut", method = RequestMethod.GET)
 	public String logOut(HttpServletRequest req) {
 		log.info("logOut: " + req);
 		return "logout";
 	}
+
+	@ResponseBody
+	@RequestMapping(value = "/PatientSearch", method = RequestMethod.GET)
+	public List<Patients> PatientSearch(
+			@RequestParam("firstname") String firstname,
+			@RequestParam("lastname") String lastname,
+			@RequestParam("cell") String cell,
+			@RequestParam("email") String email,
+			@RequestParam("address") String address,
+			@RequestParam("fileno") String fileno,
+			@RequestParam("barcode") String barcode,
+			@RequestParam("casenumber") Integer casenumber) {
+		
+		PatientsExample pe = new PatientsExample();
+		List<Patients> patient;
+		log.info("cell == " + cell + " =email= "+email+" =fileno= "+fileno );
+		if(firstname != "" || lastname != ""|| cell != "" || email != "" || address != "" || fileno != "" || barcode != "") {
+			pe.createCriteria().andFirstnameLikeInsensitive(firstname+"%").andLastnameLikeInsensitive(lastname+"%").andCellLikeInsensitive(cell+"%").andEmailLikeInsensitive(email+"%").andAddressLikeInsensitive(address+"%").andFilenoLikeInsensitive(fileno+"%").andBarcodeLikeInsensitive(barcode+"%");
+		}else if(casenumber != null) {
+			pe.createCriteria().andCasenumberEqualTo(casenumber);
+		}
+		
+		patient = pm.selectByExample(pe);
+		log.info("inside patient list" + patient);
+		return patient;
+	}
+
 }
